0f02f8a7257812e8a44468749f5e8a977fcbb96d
AdrianGR517 <159904671+AdrianGR517@users.noreply.github.com>
2024-07-09 17:36:32 +0200

    Delete MyProject directory

--- MyProject/DimensionFinita/EquivalenciaNormas.lean
+++
@@ -1,6 +0,0 @@
-import Mathlib.Analysis.Normed.Group.Lemmas
-import Mathlib.Analysis.NormedSpace.Basic
-import Mathlib.Analysis.NormedSpace.OperatorNorm.NormedSpace
-import Mathlib.LinearAlgebra.LinearIndependent
-
-import MyProject.DimensionFinita.CombLineales
